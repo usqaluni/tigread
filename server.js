6d5af011798ebe869201a5c7425ec126e34836d4
usqaluni <serov-k-o@mail.ru>
2021-12-17 02:09:27 +0300

    index file added

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const createPath = require('./helpers/create-path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-   res.sendFile('./index');
+   res.sendFile(createPath('index'));
 });
 
 app.listen(process.env.PORT, (onerror) => {

edc105c05b1172fabac64fccb837dea61abd3492
usqaluni <serov-k-o@mail.ru>
2021-12-19 16:04:22 +0300

    main pattern without transliteration

--- server.js
+++ server.js
@@ -7,16 +7,28 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-   res.render(createPath('index'));
+const urlencodedParser = express.urlencoded({extended: false});
+
+app.get('/', (request, response) => {
+    let data = {
+        text: "",
+        srclang: "russian",
+        resultlang: "hebrew",
+        transliteratedText: ""
+    };
+    response.render(createPath('index'), { data });
 });
 
-app.post('/', (req, res) => {
-  res.redirect(createPath('library'));
-});
-
-app.get('/library', (req, res) => {
-    res.render(createPath('library'));
+app.post('/', urlencodedParser, (request, response) => {
+ let data = {
+     text: request.body.srctext,
+     srclang: request.body.srclang,
+     resultlang: request.body.resultlang
+ };
+ //let transliteratedText = transliterate(data);
+    data.transliteratedText = data.text + " 333";
+    console.log(data);
+ response.render(createPath('index'), { data });
 });
 
 app.listen(PORT, (onerror) => {

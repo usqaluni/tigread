6380c253599e818404fcaff11f679d65aca911d7
usqaluni <serov-k-o@mail.ru>
2022-01-13 14:44:27 +0300

    scc test

--- public/scripts/showOriginal.js
+++ public/scripts/showOriginal.js
@@ -3,24 +3,24 @@ let showButton = document.getElementById('show');
 let transliteratedText = document.getElementById('resultText').innerText;
 let originalText = document.getElementById('originalText').innerText;
 
-let separatedText = transliteratedText.split(' ');
-let separatedOriginal = originalText.split(' ');
+let separatedText = transliteratedText.split(/[ |\n]/);
+let separatedOriginal = originalText.split(/[ |\n]/);
 
 function findWordIndex(array, index) {
       let sum = 0;
       let i = 0;
       while (sum < index) {
             sum += array[i].length + 1;
-            i++;
+            if(sum < index) i++;
       }
       return (i);
 }
-
 showButton.onclick = () => {
       if(!window.getSelection().isCollapsed){
             let selection = window.getSelection();
             let start = selection.anchorOffset;
-            M.toast({html: separatedOriginal[findWordIndex(separatedText, start)]});
+            let word = separatedOriginal[findWordIndex(separatedText, start + 1)];
+            M.toast({html: word});
       } else {
             M.toast({html: "Select some word"});
       }
